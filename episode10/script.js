749a2e0a247385bc687d7919e4dcc5774a99404a
Adam Abiyu <advmabiyu@gmail.com>
2025-11-15 20:40:18 +0700

    nambahin synchronous callback

--- episode10/script.js
+++ episode10/script.js
@@ -4,11 +4,40 @@
 //     alert(`Halo, ${nama}`);
 // }
 
-function tampilkanPesan(callback){
-    const nama = prompt('tampilkan nama: ');
-    callback(nama);
-}
+// function tampilkanPesan(callback){
+//     const nama = prompt('tampilkan nama: ');
+//     callback(nama);
+// }
 
-tampilkanPesan(nama => {
-    alert(`Halo, ${nama}`);
-});
+// tampilkanPesan(nama => {
+//     alert(`Halo, ${nama}`);
+// });
+
+// const mhs = [
+//     {
+//         "nama": "adam",
+//         "umur": "24",
+//         "idDosenWali": 1
+//     },
+//     {
+//         "nama": "budi",
+//         "umur": "22",
+//         "idDosenWali": 2
+//     },
+//     {
+//         "nama": "canto",
+//         "umur": "24",
+//         "idDosenWali": 2
+//     },
+// ]
+
+// console.log('mulai');
+// mhs.forEach(m => {
+//     for(let i = 0; i < 10000000; i++){
+//         let date = new Date();
+//     }
+//     console.log(m.nama);
+// });
+// console.log('selesai');
+
+// Asynchronous Callback
