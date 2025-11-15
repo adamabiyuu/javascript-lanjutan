0fae29d12c04b36fd33bf410c26e7c93845e3da7
Adam Abiyu <advmabiyu@gmail.com>
2025-11-15 11:41:10 +0700

    nambahin episode 9

---
+++ episode9/script.js
@@ -0,0 +1,7 @@
+// belajar tentang asynchronous Js 
+// belajar non-blocking dan blocking 
+// heap dan stack pada v8 engine pada javascript
+// js itu single threaded
+// heap itu untuk memory allocation seperti dekslarasi variabel atau function
+// kalo stack itu execution nya
+// dari stack ke web api terus ke callback queue
