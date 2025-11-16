a1244dc246d3efdb36d8dd0347232b749ce37a22
Adam Abiyu <advmabiyu@gmail.com>
2025-11-16 08:05:20 +0700

    belajar callback asynchronous with vanilla js dan jQuery hehe, agak harus ulang 3x ya ges ya

--- episode10/script.js
+++ episode10/script.js
@@ -41,3 +41,44 @@
 // console.log('selesai');
 
 // Asynchronous Callback
+
+// vanilla javascript
+// function getDataMahasiswa(url, success, error){
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = function(){
+//         if( xhr.readyState === 4){
+//             if( xhr.status === 200 ){
+//                 success(xhr.response);
+//             } else if ( xhr.status === 404 ){
+//                 error();
+//             }
+//         }
+//     }
+
+//     xhr.open('get', url);
+//     xhr.send();
+// }
+
+// console.log('mulai');
+// getDataMahasiswa('../episode10/data/mahasiswa.json', results => {
+//     const mhs = JSON.parse(results);
+//     mhs.forEach(m => console.log(m.nama));
+// }, () => {
+    
+// })
+// console.log('done');
+
+// jQuery
+console.log('mulai');
+$.ajax({
+    url: '../episode10/data/mahasiswa.json',
+    // gaperlu JSON.parse lagi kalo jQuery
+    success: (mhs) => {
+        mhs.forEach(m => console.log(m.nama));
+    },
+    error: (e) => {
+        console.log(e.responseText);
+    }
+})
+console.log('selesai');
