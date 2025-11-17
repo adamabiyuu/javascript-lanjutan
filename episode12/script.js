3fd1ca536887d71fcb51cc2d422bdec05d1ab3b0
Adam Abiyu <advmabiyu@gmail.com>
2025-11-17 23:48:05 +0700

    nambahin episode 12 jadi fetch vanilla js bukan jquery

--- episode12/script.js
+++ episode12/script.js
@@ -1,84 +1,100 @@
-// $.ajax({
-//   url: "http://www.omdbapi.com/?apikey=ec3ea753&s=avengers",
-//   success: movies => console.log(movies)
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url: "http://www.omdbapi.com/?apikey=ec3ea753&s=" + $(".input-keyword").val(),
+//     success: (results) => {
+//       const movies = results.Search;
+//       let cards = "";
+//       movies.forEach((m) => {
+//         cards += showCards(m);
+//       });
+//       // cari kelas movie container dan isi hmtl nya ganti make cards yang sudah ditumpuk tadi
+//       $(".movie-container").html(cards);
+
+//       // ketika tombol detail di-klik
+//       // cari elemen yang nama kelas nya modal-detail-button
+//       $(".modal-detail-button").on("click", function () {
+//         // console.log($(this).data('imdbid'));
+//         $.ajax({
+//           url: "http://www.omdbapi.com/?apikey=ec3ea753&i=" + $(this).data("imdbid"),
+//           success: (m) => {
+//             const movieDetail = showMovieDetail(m);
+
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
 // });
 
-// fetch("http://www.omdbapi.com/?apikey=ec3ea753&s=avengers")
-// .then(response => response.json())
-// .then(response => console.log(response));
+// fetch
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function() {
 
-// Promise
-// Object yang mempresentasikan keberhasilan / kegagalan sebuah 
-// event yang asynchronous di masa yang akan datang
-// janji (terpenuji / ingkar)
-// states (fulfilled / rejected / pending)
-// callback (resolve / reject / finally)
-// aksi (then / catch)
+  const inputKeyword = document.querySelector('.input-keyword');
+  fetch('http://www.omdbapi.com/?apikey=ec3ea753&s=' + inputKeyword.value)
+    .then((response) => response.json())
+    .then(response => {
+      const movies = response.Search;
+      let cards = '';
+      movies.forEach(m => cards += showCards(m));
+      const moviesContainer = document.querySelector('.movie-container');
+      moviesContainer.innerHTML = cards;
 
-// biasanya promise dipakai untuk request api
-// contoh 1
-// let ditepati = false;
-// const janji1 = new Promise((resolve, reject) => {
-//   if( ditepati ){
-//     resolve('ditepati');
-//   } else {
-//     reject('ingkar')
-//   }
-// });
+      const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+      modalDetailButton.forEach(btn => {
+        btn.addEventListener('click', function() {
+          const imdbid = this.dataset.imdbid;
+          fetch('http://www.omdbapi.com/?apikey=ec3ea753&i=' + imdbid)
+            .then(response => response.json())
+            .then(m => {
+              const movieDetail = showMovieDetail(m);
+              const modalBody = document.querySelector('.modal-body');
+              modalBody.innerHTML = movieDetail;
 
-// janji1
-//   .then(response => console.log('ok : ' + response))
-//   .catch(response => console.log('not ok : ' + response))
-
-// contoh 2
-// let ditepati = true;
-// const janji2 = new Promise((resolve, reject) => {
-//   if(ditepati){
-//     setTimeout(() => {
-//       resolve('ditepati setelah beberapa waktu');
-//     }, 2000);
-//   } else {
-//     setTimeout(() => {
-//       reject("ditepati setelah beberapa waktu");
-//     }, 2000);
-//   }
-// });
-
-// console.log('mulai');
-// // console.log(janji2.then(() => console.log(janji2)));
-// janji2
-//   .finally(() => console.log('selesai menunggu'))
-//   .then(response => console.log('ok : ' + response))
-//   .catch(response => console.log('not ok : ' + response))
-// console.log('selesai');
-
-// Promise.all
-const film = new Promise( resolve => {
-  setTimeout(() => {
-    resolve([{
-      judul : 'hehe',
-      sutradara: 'adam',
-      aktor: 'abiyu'
-    }])
-  }, 1000);
+              //             $(".modal-body").html(movieDetail);
+            })
+        })
+      })
+      
+    })
 })
 
-const cuaca = new Promise( resolve => {
-  setTimeout(() => {
-    resolve([{
-      kota: 'bandung',
-      temp: 26,
-      kondisi: 'cerah berawan'
-    }])
-  }, 500);
-})
 
-// film.then(response => console.log(response));
-// cuaca.then(response => console.log(response));
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+                        <div class="card" >
+                            <img src="${m.Poster}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${m.Title}</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
+                                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                            </div>
+                        </div>
+                    </div>`;
+}
 
-Promise.all([film, cuaca])
-  .then(response => {
-    const [film, cuaca] = response;
-    console.log(film);
-    console.log(cuaca);
-  })
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <img src="${m.Poster}" alt="" class="img-fluid">
+                                  </div>
+                                  <div class="col-md">
+                                    <ul class="list-group">
+                                      <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                                      <li class="list-group-item"><strong>Directors: </strong>${m.Director}</li>
+                                      <li class="list-group-item"><strong>Actors: </strong> ${m.Actors}</li>
+                                      <li class="list-group-item"><strong>Writers: </strong> ${m.Writer}</li>
+                                      <li class="list-group-item"><strong>Plot: </strong> <br> ${m.Plot}</li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>`;
+}
